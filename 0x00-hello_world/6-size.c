95ca9275a18d623d37cb775f155098c349ab1d81
toqahosney <toqa.hosney98@gmail.com>
2023-10-06 10:33:53 -0700

    4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
  * Return: Always 0 (Success)
 */
 
-int main (void)
+int main(void)
 {
 	printf("Size of a char: %u byte(s)\n", sizeof(char));
 	printf("Size of an int: %u byte(s)\n", sizeof(int));
