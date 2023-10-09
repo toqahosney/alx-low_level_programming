ec947551c91255f46450e6931b6364e3dec610ef
toqahosney <toqa.hosney98@gmail.com>
2023-10-09 16:01:58 -0700

    d

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: print alphabet in lowercase then in uppercase
+ * Description: print all alphabet letters except q and e
  *
  * Return: Always 0 (Success)
 */
@@ -11,20 +11,13 @@
 int main(void)
 {
 	char ch = 'a';
-	char CH = 'A';
 
-	/*prints a - z*/
 	while (ch <= 'z')
 	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
 		putchar(ch);
 		ch++;
-	}
-
-	/*print A -Z*/
-	while (CH <= 'Z')
-	{
-		putchar(CH);
-		CH++;
 	}
 	putchar('\n');
 
