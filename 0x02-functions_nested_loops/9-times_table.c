e14fb7ef97b50f43e91765f7d442acc77d8eea36
toqahosney <toqa.hosney98@gmail.com>
2023-10-10 18:43:21 -0700

    88

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -30,7 +30,7 @@ void times_table(void)
 			*/
 			if (prod <= 9)
 				_putchar(' ');
-			else 
+			else
 				_putchar((prod / 10) + 48);  /*get the first digit*/
 			_putchar((prod % 10) + 48); /* get the second digit*/
 		}
