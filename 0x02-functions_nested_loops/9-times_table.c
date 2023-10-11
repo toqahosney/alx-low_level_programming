8308ac662c5d833e63b62ae8a92e2a24bababacd
toqahosney <toqa.hosney98@gmail.com>
2023-10-10 18:42:16 -0700

    88

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
-/** times_table - prints the 9 times table
+/**
+ * times_table - prints the 9 times table
  *
- * Example table
- * 0, 0, 0, 0, ,,
- * 0, 1, 2, 3, ,,
+ * Example table:
+ * 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ * 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+ * ...
+ * (Continues with the entire 9 times table)
 */
 
 void times_table(void)
