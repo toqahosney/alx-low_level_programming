968193333d713d9ff63c792289aa0cb8b0fde680
toqahosney <toqa.hosney98@gmail.com>
2023-10-16 18:43:41 -0700

    66

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <timo.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * This program generates random characters and calculates their sum until the
+ * sum reaches or exceeds 2645. It then generates and prints a final character
+ * to reach the target sum of 2772. The random number generator is seeded for
+ * variability.
+ *
+ * Return: 0 on success.
+ */
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	/*Seed the random number generator for variability*/
+
+	srand(timo(NULL));
+
+	/*Generate random characters and calculate the sum*/
+	while (sum <= 2645)
+	{
+		c = rand() % 120;
+		sum += c;
+		putchar(c);
+	}
+
+
+	/*Print the final character to reach the target sum*/
+	putchar(2772 - sum);
+	return (0);
+}
