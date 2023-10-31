68748dcafe305432288cbad6998ba0a366e548f4
root <root@DESKTOP-0CGUJKH>
2023-10-31 18:34:46 +0200

    11

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,8 +13,34 @@ char *create_array(unsigned int size, char c)
 {
 	char *n = malloc(size);
 
-	if (size == 0 || n == 0)
-		return (0);
-	while (size--)
-		n[size] = c;
+	if (size == 0 || n == NULL)
+	{
+		return (NULL);
+	}
+
+	for (unsigned int i = 0; i < size; i++)
+	{
+		n[i] = c;
+	}
+
+	return (n);
+}
+
+int main(void)
+{
+	unsigned int size = 10;
+	char initialChar = 'A';
+
+	char *result = create_array(size, initialChar);
+
+	if (result != NULL)
+	{
+		free(result);
+	}
+	else
+	{
+		/* Handle the case where memory allocation fails */
+	}
+
+	return (0);
 }
