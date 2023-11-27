2505baa0362e37bb13cc780e8839190b56c8d745
root <root@DESKTOP-0CGUJKH>
2023-11-27 12:19:22 +0200

    55

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,8 @@
 #include"main.h"
 
 /**
- * read_textfile - Reads and prints the content of a file to the standard output.
+ * read_textfile - Reads and prints the content
+ * of a file to the standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters (bytes) to read and print.
  *
@@ -19,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return(0);
+		return (0);
 	bytes = read(fd, &buf[0], letters);
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
